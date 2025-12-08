f5b06c195a412c1bb069d2c52fd071c7a2ba8ece
Ashley Connor <me@ashleyconnor.co.uk>
2025-12-07 16:04:12 -0800

    Initial Commit

---
+++ Torrent Editor/Torrent_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Torrent_EditorApp.swift
+//  Torrent Editor
+//
+//  Created by Ashley Connor on 07/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Torrent_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Torrent EditorTests/Torrent_EditorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Torrent_EditorTests.swift
+//  Torrent EditorTests
+//
+//  Created by Ashley Connor on 07/12/2025.
+//
+
+import Testing
+@testable import Torrent_Editor
+
+struct Torrent_EditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
